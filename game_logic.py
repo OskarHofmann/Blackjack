72380c0348430239db785b10a512ad13567bdac9
Oskar Hofmann <oskar.hofmann@gmx.de>
2023-04-07 20:22:37 +0200

    Add split check and first code for user input

--- game_logic.py
+++ game_logic.py
@@ -25,7 +25,8 @@ class Hand():
 
     def __str__(self):
         cards = [str(card) for card in self.cards]
-        return ", ".join(cards)
+        card_str = ", ".join(cards)
+        return card_str + f' ({self.get_points()})'
 
     def draw_card(self, shoe: cards.Deck = None):
         if shoe:
@@ -50,6 +51,13 @@ class Hand():
                 n_aces -= 1              
 
         return total
+    
+    def is_splittable(self): 
+        # hand is splittable if and only if it contains two cards with the same value
+        if len(self.cards) != 2:
+            return False
+        return (POINT_VALUES[self.cards[0].value.name] == POINT_VALUES[self.cards[1].value.name])
+
     
 
 
@@ -76,6 +84,11 @@ class Game():
 
         
     class Round():
+
+        # TODO: Move to UI and define game_Logic constants as return values
+        PLAYER_CHOICES_WITHOUT_SPLIT = ['1: Draw', '2: Hold']
+        PLAYER_CHOICES_WITH_SPLIT = ['1: Draw', '2: Hold', '3: Split']
+
         def __init__(self, shoe: cards.Deck = None):
             # a player can have several hands when he splits, the dealer always has 1 hand
             self.player_hands = [Hand()]
@@ -85,14 +98,26 @@ class Game():
             self.player_hands[0].draw_card(self.shoe)
             self.dealer_hand.draw_card(self.shoe)
             self.player_hands[0].draw_card(self.shoe)
-            self.dealer_hand.draw_card(self.shoe)
+            
+            # The dealer would get his 2nd card upside down at this point, but it does not matter if
+            # he draws it now or later (during play()).
+            # Drawing it later also allows to use standard __str__ method the dealer's hand 
+            # self.dealer_hand.draw_card(self.shoe)
 
         def play(self):                   
-            #self.UI.update()
-            print(self.player_hands[0])
-            print(self.dealer_hand.cards[0])
+            # self.UI.update()
+            print("Player: ", self.player_hands[0])
+            print("Dealer: ", self.dealer_hand)
 
             for player_hand in self.player_hands:
+                # self.UI.user_input()
+                choice_str = '1: Draw \n2:Hold'
+                if player_hand.is_splittable():
+                    choice_str += '\n'
+                while True:
+
+                    choice = int(input()
+
                 pass    
 
             return False, 0
